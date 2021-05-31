ddc3b52551950c1ccc3414304488e3c902cc2a86
michaelzzm <michael19851@hotmail.com>
2021-05-31 15:13:15 +0800

    Update color_cosmetics.js
    Major change:
    To use an array instead of one by one when inserting data

--- color_cosmetics.js
+++ color_cosmetics.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http*://ftba.nmpa.gov.cn:8181/*
+// @match        http*://ftba.nmpa.gov.cn:8181/ftban/fw.jsp
 // @grant        none
 // @require      https://unpkg.com/ajax-hook@2.0.3/dist/ajaxhook.min.js
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
@@ -16,10 +16,12 @@ ah.proxy({
     //请求发起前进入
     onRequest: (config, handler) => {
         // console.log(config.url)
-        if (config.body.substr(0,2) == 'on'){
-            let res_body = config.body
-            let cur_page = res_body.split('&')[res_body.split('&').length-1]
-            console.log(cur_page, 'begin')
+        if (config.body != null) {
+            if (config.body.substr(0,2) == 'on'){
+                let res_body = config.body
+                let cur_page = res_body.split('&')[res_body.split('&').length-1]
+                console.log(cur_page, 'begin')
+            }
         }
         handler.next(config);
     },
@@ -30,15 +32,20 @@ ah.proxy({
     },
     //请求成功后进入
     onResponse: (response, handler) => {
-        if (response.config.body.substr(0,2) == 'on'){
-            let res_body = response.config.body
-            let cur_page = res_body.split('&')[res_body.split('&').length-1]
+        if (response.config.body != null) {
+            if (response.config.body.substr(0,2) == 'on'){
+                let res_body = response.config.body
+                let cur_page = res_body.split('&')[res_body.split('&').length-1]
 
-            console.log(cur_page, 'doing')
+                console.log(cur_page, 'doing')
+            }
         }
 
         let result = JSON.parse(response.response)
         let company_list = result.list
+
+        var dict_records = []
+
         for (let item in company_list) {
             let cur_line = company_list[item]
             let off = item.is_off =="Y" ? "（已注销）":"";
@@ -46,16 +53,26 @@ ah.proxy({
             let cur_url = baseURL + "itownet/hzp_ba/fw/pz.jsp?processid=" + cur_line.processid + "&nid=" + cur_line.newProcessid
             let cur_enterpriseName = cur_line.enterpriseName+"（" + cur_line.apply_enter_address+"）"
             // console.log(cur_url, cur_applySn, cur_enterpriseName, cur_line.newProcessid, cur_line.productName, cur_line.provinceConfirm, cur_line.apply_enter_address)
-            //发送到数据库中
-            $.ajax({
-                type: 'POST',
-                url: "http://127.0.0.1:5000/postDummydata",
-                data: {'applySn': cur_applySn, 'url': cur_url, 'enterpriseName': cur_enterpriseName, 'date': cur_line.provinceConfirm, 'productname': cur_line.productName},
-                success: function(res) {
-                    // console.log('done');
-                }
-            });
+            //发送到dis_pharma_website的数据库中
+
+            dict_records.push({'applySn': cur_applySn, 'url': cur_url, 'enterpriseName': cur_enterpriseName, 'date': cur_line.provinceConfirm, 'productname': cur_line.productName});
+            // $.ajax({
+            //     type: 'POST',
+            //     url: "http://127.0.0.1:5000/postDummydata",
+            //     data: {'applySn': cur_applySn, 'url': cur_url, 'enterpriseName': cur_enterpriseName, 'date': cur_line.provinceConfirm, 'productname': cur_line.productName},
+            //     success: function(res) {
+            //         // console.log('done');
+            //     }
+            // });
         }
+        $.ajax({
+            type: 'POST',
+            url: "http://127.0.0.1:5000/postDummydata",
+            data: {'records': dict_records},
+            success: function(res) {
+                // console.log('done');
+            }
+        });
         handler.next(response)
     }
 })
